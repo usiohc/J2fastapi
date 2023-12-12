605e8346f2424b15a23ab09b9dafde03d8b01979
usiohc <asltn99@gmail.com>
2023-12-12 15:35:59 +0900

    ch_2-09 | 질문, 답변 부트스트랩 간단 적용
    Signed-off-by: usiohc <asltn99@gmail.com>

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -4,20 +4,30 @@
 
     let question_list = []
 
-    function get_question_list() {
-        fastapi('get', '/api/question/list', {}, (json) => {
-            question_list = json
-        })
-    }
-    
-    get_question_list()
-    
-    
-  </script>
-  
-  <ul>
-    {#each question_list as question}
-        <li><a use:link href="/detail/{question.id}">{question.subject}</a></li>
-    {/each}
-  </ul>
-  
+    fastapi('get', '/api/question/list', {}, (json) => {
+        question_list = json
+    })
+</script>
+
+<div class="container my-3">
+    <table class="table">
+        <thead>
+        <tr class="table-dark">
+            <th>번호</th>
+            <th>제목</th>
+            <th>작성일시</th>
+        </tr>
+        </thead>
+        <tbody>
+        {#each question_list as question, i}
+        <tr>
+            <td>{i+1}</td>
+            <td>
+                <a use:link href="/detail/{question.id}">{question.subject}</a>
+            </td>
+            <td>{question.create_date}</td>
+        </tr>
+        {/each}
+        </tbody>
+    </table>
+</div>
