982183d85a5d1344952a79824f2a0fef6228e05d
usiohc <asltn99@gmail.com>
2023-12-14 12:56:07 +0900

    ch_3-01 | 내비게이션바
    Signed-off-by: usiohc <asltn99@gmail.com>

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,14 +1,18 @@
 <script>
-  import Router from 'svelte-spa-router'
-  import Home from "./routes/Home.svelte"
-  import Detail from "./routes/Detail.svelte"
-  import QuestionCreate from "./routes/QuestionCreate.svelte"
+    import Router from 'svelte-spa-router'
+    import Home from "./routes/Home.svelte"
+    import Detail from "./routes/Detail.svelte"
+    import QuestionCreate from "./routes/QuestionCreate.svelte"
+    import Navigation from './components/Navigation.svelte';
 
-  const routes = {
-    '/': Home,
-    '/detail/:question_id': Detail,
-    '/question-create': QuestionCreate,
-  }
+
+    const routes = {
+      '/': Home,
+      '/detail/:question_id': Detail,
+      '/question-create': QuestionCreate,
+    }
+    
 </script>
 
+<Navigation />
 <Router {routes}/>
